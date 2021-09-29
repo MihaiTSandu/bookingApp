6e839034f8ecaba06da1650e49878086b979d7cb
MihaiTSandu <stmihai98@gmail.com>
2021-09-29 19:28:51 +0300

    first commit

---
+++ src/auth/Register.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const Register = () => {
+  <div className="container-fluid p-5 text-center">Register Page</div>;
+};
+export default Register;
---
+++ src/booking/Home.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const Home = () => {
+  <div className="container-fluid p-5 text-center">Home Page</div>;
+};
+export default Home;
