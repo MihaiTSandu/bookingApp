96c344483dddbb1541f4ae4b48257501659ce8b6
MihaiTSandu <stmihai98@gmail.com>
2021-09-30 12:35:16 +0300

    feat: add top navigation bar

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,10 +2,12 @@ import Login from "app/pages/auth/Login";
 import Register from "app/pages/auth/Register";
 import Home from "app/pages/booking/Home";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import TopNav from "./components/TopNav";
 
 function App() {
   return (
     <BrowserRouter>
+      <TopNav />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/login" component={Login} />
