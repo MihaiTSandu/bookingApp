191ee4a09576ed3ac21bd6e9adc42164ccd57936
MihaiTSandu <stmihai98@gmail.com>
2021-10-07 16:29:52 +0300

    feat: add register form

--- src/app/pages/auth/Register/index.tsx
+++ src/app/pages/auth/Register/index.tsx
@@ -1,10 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.table({ name, email, password });
+  };
+
+  const registerForm = () => (
+    <form onSubmit={handleSubmit} className="mt-3">
+      <div className="form-group mb-3">
+        <label className="form-label mb">Your name</label>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Enter name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+
+      <div className="form-group mb-3">
+        <label className="form-label mb">Email address</label>
+        <input
+          type="email"
+          className="form-control"
+          placeholder="Enter e-mail"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+
+      <div className="form-group mb-3">
+        <label className="form-label mb">Password</label>
+        <input
+          type="password"
+          className="form-control"
+          placeholder="Enter password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+
+      <button className="btn btn-primary">Submit</button>
+    </form>
+  );
+
   return (
-    <div className="container-fluid p-5 text-center">
-      <h1>Register Page</h1>
-    </div>
+    <>
+      <div className="container-fluid bg-secondary p-5 text-center">
+        <h1>Register Page</h1>
+      </div>
+
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6 offset-md-3">{registerForm()}</div>
+        </div>
+      </div>
+    </>
   );
 };
 export default Register;
